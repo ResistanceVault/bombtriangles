9fd1d65e221bd8026dbd6ffecee97486d418ade0
Ozzyboshi <gun101@email.it>
2022-04-10 00:29:47 +0200

    more spaceship assets

--- spaceship/spaceshipmanager.s
+++ spaceship/spaceshipmanager.s
@@ -1,10 +1,21 @@
+SPACESHIP_DECIMALDIGITS EQU 64
+SPACESHIP_START_X       EQU 64
+SPACESHIP_START_Y       EQU 44
+SPACESHIP_DESTINATION_X EQU 160
+SPACESHIP_DESTINATION_Y EQU 100
+SPACESHIP_SPRITE_HEIGHT EQU 10
+
+SPACESHIP_SET_NEW_DESTINATION MACRO
+  move.l #((SPACESHIP_DECIMALDIGITS*2*\1)<<16)|\2,SPACESHIPDESTINATIONPOSITION
+  ENDM
+
 SPACESHIPCURRENTPOSITION:
-  dc.w  $2400                 ;X current position
-  dc.w  $0c00                 ;Y current position
+  dc.w  2*SPACESHIP_START_X*SPACESHIP_DECIMALDIGITS           ;X current position
+  dc.w  SPACESHIP_START_Y*SPACESHIP_DECIMALDIGITS             ;Y current position
 
 SPACESHIPDESTINATIONPOSITION:
-  dc.w    $2400               ;X destination position
-  dc.w    $2f00               ;Y destination position
+  dc.w  2*SPACESHIP_DESTINATION_X*SPACESHIP_DECIMALDIGITS    ;X destination position
+  dc.w  SPACESHIP_DESTINATION_Y*SPACESHIP_DECIMALDIGITS      ;Y destination position
 
 SPACESHIPDIRECTIONVECTOR:
   dc.l                   0
@@ -34,7 +45,8 @@ SPACESHIPMANAGER:
   tst.w SPACESHIPDIRECTIONVECTOR+2
   bne.s nogotonextlocation
   move.l SPACESHIPDESTINATIONPOSITION,SPACESHIPCURRENTPOSITION
-  move.l #$23002f00,SPACESHIPDESTINATIONPOSITION
+  ;move.l #$23002f00,SPACESHIPDESTINATIONPOSITION
+  SPACESHIP_SET_NEW_DESTINATION 64,150
   move.w #1,DESTINATION_REACHED
   movem.l                (sp)+,d0-d7/a0-a6
   rts
@@ -59,30 +71,30 @@ nogotonextlocation:
   move.w d0,d1
   swap d0
 
- 
+  ; if d0 is odd we are moving the spaceship to an odd location, in this case we must set
+  btst #0,d0
+  beq.s spaceship_no_odd_x
+  bset #0,3+SPACESHIP1_BPL0
+  bset #0,3+SPACESHIP1_BPL1
+  bra.s spaceship_place_coords
+spaceship_no_odd_x:
+  bclr #0,3+SPACESHIP1_BPL0
+  bclr #0,3+SPACESHIP1_BPL1
+spaceship_place_coords:
 
-  lsr.w #6,d0
+  ; normalize for sprite
+  lsr.w #7,d0
   lsr.w #6,d1
 
-
-
- 
   DEBUG 1234
-
-
-
-  ;moveq #$80,d0
-  ;swap d0
 
   move.b d0,SPACESHIP1_BPL0_HSTART
   move.b d0,SPACESHIP1_BPL1_HSTART
-  ;rts
 
-  ;swap d0
   move.b d1,SPACESHIP1_BPL0_VSTART
   move.b d1,SPACESHIP1_BPL1_VSTART
 
-  add.w #10,d1
+  add.w #SPACESHIP_SPRITE_HEIGHT,d1
 
   move.b d1,SPACESHIP1_BPL0_VSTOP
   move.b d1,SPACESHIP1_BPL1_VSTOP
