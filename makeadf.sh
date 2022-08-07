427dac32493cfdf70fec7ae8ccbc59310983b4ef
Ozzyboshi <gun101@email.it>
2022-08-07 14:53:48 +0200

    ks 1.x compat

--- makeadf.sh
+++ makeadf.sh
@@ -1,1 +1,3 @@
-exe2adf-linux64bit -i rcompresso -l flash2022_$(date +'%F_%T') -a flash2022_$(date +'%F_%T').adf
+exe2adf-linux64bit -i rcompresso -l flash2022compresso_$(date +'%F_%T') -a flash2022compresso_$(date +'%F_%T').adf
+exe2adf-linux64bit -i reffect -l flash2022effect_$(date +'%F_%T') -a flash2022effect_$(date +'%F_%T').adf
+exe2adf-linux64bit -i r -l flash2022r_$(date +'%F_%T') -a flash2022r_$(date +'%F_%T').adf
