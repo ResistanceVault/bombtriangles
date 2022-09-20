8f6e4ab9225a7ca1a9cb46303183d7c9f1d24e91
Ozzyboshi <gun101@email.it>
2022-09-20 22:20:40 +0200

    ready for flashparty
    (cherry picked from commit ce5937b3041bfa55b846849af544491aa5f0dcbe)

--- makeadf.sh
+++ makeadf.sh
@@ -1,3 +1,4 @@
-exe2adf-linux64bit -i rcompresso -l flash2022compresso_$(date +'%F_%T') -a flash2022compresso_$(date +'%F_%T').adf
+cp rcompresso bombtriangles
+exe2adf-linux64bit -i bombtriangles -l bombtriangles_$(date +'%F_%T') -a bombtriangles_$(date +'%F_%T').adf
 exe2adf-linux64bit -i reffect -l flash2022effect_$(date +'%F_%T') -a flash2022effect_$(date +'%F_%T').adf
 exe2adf-linux64bit -i r -l flash2022r_$(date +'%F_%T') -a flash2022r_$(date +'%F_%T').adf
